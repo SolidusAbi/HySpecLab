879cfc1356ec6c9acd752cdbaaf17ef7cafae466
Abián Hernández <abianhernandez@outlook.es>
2023-03-01 14:04:29 +0000

    Contrasitve Learning method for Unmixing improved

--- HySpecLab/unmixing/ContrastiveUnmixing.py
+++ HySpecLab/unmixing/ContrastiveUnmixing.py
@@ -1,14 +1,14 @@
 import torch 
 import torch.nn as nn
 from torch.nn import Parameter
-from torch.nn.functional import softmax
+from torch.nn.functional import softmax, normalize
 
 from ..metrics import sad
 from .mixture import lmm
 from .utils import slide
 
 class ContrastiveUnmixing(nn.Module):
-    def __init__(self, n_bands, n_endmembers, encode_layers=[1024, 512, 128, 32], endmember_init=None) -> None:
+    def __init__(self, n_bands, n_endmembers, encode_layers=[512, 128, 32], endmember_init=None) -> None:
         super(ContrastiveUnmixing, self).__init__()      
         encode_layers = [n_bands] + encode_layers
         
@@ -31,7 +31,8 @@ class ContrastiveUnmixing(nn.Module):
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         r0 = self.projection( self.encoder(input) )
-        self.A = sad(r0, self.ebk)
+
+        self.A = self.__similarity(r0)
         return lmm(softmax(self.A, dim=1), torch.sigmoid(self.ebk))
 
     def __encode_layer(self, in_features, out_features, dropout=False):
@@ -40,3 +41,25 @@ class ContrastiveUnmixing(nn.Module):
             nn.BatchNorm1d(out_features),
             *[nn.ReLU(), nn.Dropout(0.5)] if dropout else [nn.Identity()]
         )
+
+    def __similarity(self, X: torch.Tensor, temperature=1e-1) -> torch.Tensor:
+        '''
+            Cosine similarity between input and endmember bank.
+
+            Parameters
+            ----------
+                x: torch.Tensor, shape=(batch_size, n_bands)
+                    input tensor.
+                
+                temperature: float, default=1e-1
+                    temperature parameter for contrastive learning.
+                
+        '''
+        bs, n_bands = X.shape
+        X = normalize(X, dim=1)
+
+        normalize_ebk = normalize(self.ebk.detach(), dim=1).expand(bs, -1, -1)
+        cos = torch.bmm(X.view(bs, 1, n_bands), torch.transpose(normalize_ebk, 1, 2)).squeeze()
+        return (1 - torch.pow(cos, 2))/temperature
+
+        
